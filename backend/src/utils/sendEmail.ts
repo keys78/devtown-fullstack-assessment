d89df9d98cc64ee9042644fa79a94093424b467a
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-07-21 12:50:53 +0100

    fix: vercel 404 errors

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer'
 
-const sendEmail = async (options: any) => {
+const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         host: 'smtp.gmail.com',
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://devtown-notesapp-api.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
